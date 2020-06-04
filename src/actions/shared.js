c8226dd04b039f851211e45db72135decf491d17
aelabd <elabd1996@gmail.com>
2020-06-04 17:51:16 +0200

    New Tweet component & loading bar added

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,15 +2,17 @@ import { getInitialData } from "../utils/api";
 import { recieveUsers } from "../actions/users";
 import { recieveTweets } from "../actions/tweets";
 import { setAuthUser } from "../actions/authedUser";
-
+import {showLoading , hideLoading} from 'react-redux-loading'
 const AUTHED_ID = "tylermcginnis";
 
 export function handleInitialData() {
   return dispatch => {
+    dispatch(showLoading());
     return getInitialData().then(({ users, tweets }) => {
       dispatch(recieveUsers(users));
       dispatch(recieveTweets(tweets));
       dispatch(setAuthUser(AUTHED_ID));
+      dispatch(hideLoading());
     });
   };
 }
--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,22 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { handleInitialData } from "../actions/shared";
 import Dashboard from "./Dashboard";
-
+import LoadingBar from "react-redux-loading";
+import NewTweet from "./NewTweet";
 class App extends Component {
   componentDidMount() {
-    console.log(this.props);
+    // console.log(this.props);
     this.props.dispatch(handleInitialData());
   }
 
   render() {
-    return <div>{this.props.loading === true ? null : <Dashboard />}</div>
+    return (
+      <div>
+        <LoadingBar />
+        <NewTweet/>
+        {this.props.loading === true ? null : <Dashboard />}
+      </div>
+    );
   }
 }
 
