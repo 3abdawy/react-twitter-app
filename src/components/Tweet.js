e884e00586a18bcb2a425a43a0322e6ecb4acc4a
aelabd <elabd1996@gmail.com>
2020-06-06 20:49:29 +0200

    Toggletweet is added

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -4,7 +4,7 @@ import { formatTweet, formatDate } from "../utils/helpers";
 import { TiArrowBackOutline } from "react-icons/ti/index";
 import { TiHeartFullOutline } from "react-icons/ti/index";
 import { TiHeartOutline } from "react-icons/ti/index";
-
+import { handleToggleTweet } from "../actions/tweets";
 class Tweet extends Component {
   toParent = (e, id) => {
     e.preventDefault();
@@ -13,7 +13,14 @@ class Tweet extends Component {
 
   handleLike = e => {
     e.preventDefault();
-    //todo: handle like tweet
+
+    const { dispatch, tweet, authedUser } = this.props;
+
+    dispatch(handleToggleTweet({
+      id:tweet.id,
+      hasLiked: tweet.hasLiked,
+      authedUser:authedUser
+    }));
   };
   render() {
     console.group("Tweet");
